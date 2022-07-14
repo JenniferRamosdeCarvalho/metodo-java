36aba702ce7c3ba780e3b6e1698bc96267c6236c
Perkles <jenniferramosdecarvalho@gmail.com>
2022-07-14 09:35:46 -0300

    métodos java

---
+++ src/metodo/java/Main.java
@@ -0,0 +1,16 @@
+package metodo.java;
+
+public class Main {
+    public static void main(String [] args) {
+        System.out.println ("Exercício Métodos");
+        metodosexercicios.soma (3,6);
+
+        System.out.println("Exercício mensagem");
+        comprimento.obterMensagem(9);
+
+        System.out.println("Exercício empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+
+
+    }
+}
---
+++ src/metodo/java/metodosexercicios.java
@@ -0,0 +1,10 @@
+package metodo.java;
+
+public class metodosexercicios {
+    public static void soma(double numero1, double numero2) {
+        double resultado = numero1 + numero2;
+
+        System.out.println("A soma de" + numero1 + "mais" + numero2 + "é" + resultado);
+
+    }
+}
